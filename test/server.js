9eddcf7cd1cb604a8104853545013d1609c69dd6
priyankajadhav22 <75733218+priyankajadhav22@users.noreply.github.com>
2020-12-10 16:27:40 +0530

    Add files via upload

---
+++ test/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+//server configuration
+app.use(express.static(path.join(__dirname)));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:8081", host, port)
+})
